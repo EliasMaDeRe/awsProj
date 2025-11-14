c3a4c8eba11d7009f4d65473998f2789456d1d02
Elías Madera <97937391+EliasMaDeRe@users.noreply.github.com>
2025-11-13 20:34:07 -0600

    Change server running message in app.py
    Updated the print statement to indicate the server is running.

--- app.py
+++ app.py
@@ -89,5 +89,7 @@ def delete_profesor(profesor_id):
     return jsonify({"error": "Profesor no encontrado"}), 404
 
 if __name__ == '__main__':
+    print(f"[OK] Servidor Flask corriendo en http://0.0.0.0:3000")
     app.run(host='0.0.0.0', port=3000)
+
 
